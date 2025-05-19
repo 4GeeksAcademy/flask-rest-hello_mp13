55c458627d42745ca9382662e5b235e3fe7fa5a1
Marco Pastene <mpastene1996@outlook.com>
2025-05-19 17:30:39 +0000

    marcopastene13

--- src/models.py
+++ src/models.py
@@ -1,19 +1,69 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, Integer, String, ForeignKey, Enum
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    firstname = Column(String(50))
+    lastname = Column(String(50))
+    email = Column(String(100), unique=True, nullable=False)
 
+    posts = relationship("Post", back_populates="user")
+    comments = relationship("Comment", back_populates="author")
+    followers = relationship("Follower", foreign_keys='Follower.user_to_id')
+    following = relationship("Follower", foreign_keys='Follower.user_from_id')
+    likes = relationship("Like", back_populates="user")
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Follower(Base):
+    __tablename__ = 'follower'
+
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    user = relationship("User", back_populates="posts")
+    media = relationship("Media", back_populates="post")
+    comments = relationship("Comment", back_populates="post")
+    likes = relationship("Like", back_populates="post")
+
+class Media(Base):
+    __tablename__ = 'media'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum("image", "video", name="media_type"))
+    url = Column(String(255))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    post = relationship("Post", back_populates="media")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(500))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    author = relationship("User", back_populates="comments")
+    post = relationship("Post", back_populates="comments")
+
+class Like(Base):
+    __tablename__ = 'like'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    user = relationship("User", back_populates="likes")
+    post = relationship("Post", back_populates="likes")
